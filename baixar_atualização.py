6353f2ff0575431533727128175151e9b929b119
RogerioCabral1971 <rogeriocabral1971@gmail.com>
2024-08-08 20:36:13 -0300

    Add files via upload

--- baixar_atualização.py
+++ baixar_atualização.py
@@ -1,4 +1,14 @@
 from git import Repo
+import extrair_informacoes as ext
+import shutil
+import datetime
 
+today=datetime.date.today()
+dia=today.strftime('%d/%m/%Y')
+
+dire=ext.ler_toml()['pastas']['dir']
 repo_url = 'https://github.com/RogerioCabral1971/plenoled'
-local_dir = 'arduino'
+local_dir = fr'{dire}app'
+
+shutil.copytree(fr'{dire}app', fr'{dire}app{dia}')
+repo = Repo.clone_from(repo_url, local_dir)
